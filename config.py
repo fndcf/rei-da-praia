4b93f97a409c5adec2d4edeaac016cfb3158a0a5
Fernando Carvalho <fernando.carvalhof@hotmail.com>
2025-07-08 20:49:11 -0300

    ajuste no config do sql

--- config.py
+++ config.py
@@ -8,3 +8,8 @@ class Config:
     TEMPLATES_AUTO_RELOAD = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+    'pool_pre_ping': True,
+    'pool_recycle': 300,
+    'connect_args': {'sslmode': 'require'}
+    }
